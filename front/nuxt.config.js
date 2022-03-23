550a82170e4bb3f72214b32dca1f94d9b53cb3d3
tokku5552 <physics.0121@gmail.com>
2022-03-23 09:50:06 +0900

    setup axios

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -41,7 +41,12 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/axios'],
+
+  axios: {
+    baseURL: 'http://localhost:8000'
+    // proxy: true
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
