d0310f569a2c11a17c6ff7237829408dd7c26833
tokku5552 <physics.0121@gmail.com>
2022-03-25 23:34:35 +0900

    構成図作成

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,7 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStack extends Stack {
+export class NuxtRailsSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
