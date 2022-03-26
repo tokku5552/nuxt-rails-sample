8b524a1815b4bbff9b8ba1be828cacd270d2cef5
tokku5552 <physics.0121@gmail.com>
2022-03-26 23:40:07 +0900

    wip : create ec2

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,13 +2,24 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as iam from 'aws-cdk-lib/aws-iam';
-// import * as keypair from 'cdk'
 
 export class NuxtRailsSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'RailsVPC');
+    const vpc = new ec2.Vpc(this, 'RailsVPC', {
+      cidr: '10.0.0.0/21',
+      subnetConfiguration: [{
+        subnetType: ec2.SubnetType.PUBLIC,
+        name: 'Ingress',
+        cidrMask: 24
+      },
+      {
+        cidrMask: 24,
+        name: 'Database',
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      }],
+    });
 
     const keyName = process.env.KEY_NAME;
 
@@ -32,14 +43,33 @@ export class NuxtRailsSampleStack extends Stack {
 
     new ec2.Instance(this, 'RailsInstance', {
       vpc,
+      instanceName: 'RailsInstance',
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
       machineImage: new ec2.AmazonLinuxImage({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
       }),
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
       securityGroup: securityGroup,
       keyName: keyName,
-      role: role
+      role: role,
+      // init: ec2.CloudFormationInit.fromConfigSets({
+      //   configSets: {
+      //     default: ['yumPreinstall', 'config'],
+      //   },
+      //   configs: {
+      //     yumPreinstall: new ec2.InitConfig([
+      //       ec2.InitPackage.yum('git'),
+      //       ec2.InitPackage.yum('nginx')
+      //     ])
+      //   }
+      // })
     })
+    // const handle = new ec2.InitServiceRestartHandle();
+    // ec2.CloudFormationInit.fromElements(
+    //   ec2.InitFile.fromString('/etc/nginx/nginx.conf', '', { serviceRestartHandles: [handle] })
+    // )
 
   }
 }
