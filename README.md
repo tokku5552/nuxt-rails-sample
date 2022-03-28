b1040d9d2d137a43953462efb7af13274e455e48
tokku5552 <physics.0121@gmail.com>
2022-03-29 00:31:43 +0900

    RDS追加

--- README.md
+++ README.md
@@ -17,3 +17,44 @@ make up
 make app
 ```
 http://localhost:8000/
+
+## 本番環境
+- パラメータストアにDBのパスワードを保存
+```
+aws ssm put-parameter --name "RailsApiRDS" --value "your_db_password" --type "SecureString"
+```
+
+- cdk手動デプロイ
+```
+npx cdk deploy --all
+```
+
+- EC2へSSM経由でSSH
+```
+ssh ec2-user@<your_ec2_instance_id> -i <your_private_key>
+```
+
+- EC2でのセットアップ
+```
+sudo yum -y update
+sudo yum -y install git make gcc-c++ patch openssl-devel libyaml-devel libffi-devel libicu-devel libxml2 libxslt libxml2-devel libxslt-devel zlib-devel readline-devel ImageMagick ImageMagick-devel
+sudo amazon-linux-extras install -y nginx1
+sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo yum -y remove mariadb-libs
+sudo yum localinstall -y https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+sudo yum-config-manager --disable mysql80-community
+sudo yum-config-manager --enable mysql57-community
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+sudo yum -y install mysql-community-client mysql-server mysql-devel
+```
+
+- RDSへ接続
+```
+mysql -h <your_rds_endpoint> -u api -p
+```
+
+- DBのパスワードを取得
+```
+aws ssm get-parameter --name "RailsApiRDS" --with-decryption
+```
--- cdk/lib/rails-sample-stack.ts
+++ cdk/lib/rails-sample-stack.ts
@@ -1,14 +1,16 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, SecretValue } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { InstanceTarget } from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets';
+import * as rds from 'aws-cdk-lib/aws-rds';
 
 export class RailsSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // create VPC
     const vpc = new ec2.Vpc(this, 'RailsVPC', {
       cidr: '10.0.0.0/21',
       subnetConfiguration: [{
@@ -23,19 +25,19 @@ export class RailsSampleStack extends Stack {
       }],
     });
 
-    const keyName = process.env.KEY_NAME;
-
-    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+    const securityGroupforEC2 = new ec2.SecurityGroup(this, 'SecurityGroupforEC2', {
       vpc,
       description: 'Allow HTTP ingress',
       allowAllOutbound: true,
     })
 
-    securityGroup.addIngressRule(
+    securityGroupforEC2.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(80),
       "Allow HTTP Access"
     )
+
+    // IAM role for SSM Agent
     const role = new iam.Role(this, "ec2Role", {
       assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
     });
@@ -43,6 +45,8 @@ export class RailsSampleStack extends Stack {
       iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
     );
 
+    // create EC2 Instance
+    const keyName = process.env.KEY_NAME;
     const instance = new ec2.Instance(this, 'RailsInstance', {
       vpc,
       instanceName: 'RailsInstance',
@@ -53,15 +57,16 @@ export class RailsSampleStack extends Stack {
       vpcSubnets: {
         subnetType: ec2.SubnetType.PUBLIC,
       },
-      securityGroup: securityGroup,
+      securityGroup: securityGroupforEC2,
       keyName: keyName,
       role: role,
     })
 
+    // create ALB
     const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
       vpc,
       internetFacing: true,
-      securityGroup: securityGroup
+      securityGroup: securityGroupforEC2
     })
 
     const listener = lb.addListener('Listener', {
@@ -75,5 +80,29 @@ export class RailsSampleStack extends Stack {
     })
 
     // RDS
+    const securityGroupforRDS = new ec2.SecurityGroup(this, 'SecurityGroupforRDS', {
+      vpc,
+      description: 'Allow MySQL ingress',
+      allowAllOutbound: true,
+    })
+
+    securityGroupforRDS.addIngressRule(
+      ec2.Peer.securityGroupId(securityGroupforEC2.securityGroupId),
+      ec2.Port.tcp(3306),
+      "Allow MySQL Access"
+    )
+    const engine = rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_5_7_34 })
+    const db = new rds.DatabaseInstance(this, 'RailsRDS', {
+      engine,
+      vpc,
+      databaseName: 'api_production',
+      instanceIdentifier: 'railsrds',
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      credentials: rds.Credentials.fromPassword('api', SecretValue.ssmSecure('RailsApiRDS', '1')),
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      securityGroups: [securityGroupforRDS]
+    })
   }
 }
