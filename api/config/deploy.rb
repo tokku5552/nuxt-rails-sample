3b703ac7fe500aeaee8a4ee439169f0a1bbfac6f
tokku5552 <physics.0121@gmail.com>
2022-04-02 22:03:06 +0900

    fix branch

--- api/config/deploy.rb
+++ api/config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.17.0"
 set :application, "api"
 set :repo_url, "git@github.com:tokku5552/nuxt-rails-sample.git"
 set :rbenv_ruby, File.read('.ruby-version').strip
-set :branch, ENV['BRANCH'] || "hotfix/fix-secupdate-puma"
+set :branch, ENV['BRANCH'] || "main"
 set :nginx_config_name, "#{fetch(:application)}.conf"
 set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
 set :deploy_to, '/var/www/api'
